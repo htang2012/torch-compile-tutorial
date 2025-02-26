74c6625fe65568a6778fb4a05a34f59a33ad7492
Henry Tang <ytang@habana.ai>
2025-02-26 10:30:45 -0800

    add shebang on the launch.sh.

--- HPU/launch.sh
+++ HPU/launch.sh
@@ -1,1 +1,2 @@
- PT_HPU_LAZY_MODE=0 python -Xfrozen_modules=off -m debugpy --listen localhost:5678 --wait-for-client main_torch_compile.py --device hpu --torch_compile_type hpubackend
+#!/usr/bin/bash
+PT_HPU_LAZY_MODE=0 python -Xfrozen_modules=off -m debugpy --listen localhost:5678 --wait-for-client main_torch_compile.py --device hpu --torch_compile_type hpubackend
